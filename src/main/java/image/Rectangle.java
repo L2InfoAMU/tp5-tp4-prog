9ae9b3eec241a2be253991961044372175005f5e
m15016725 <guillaume.montagner@etu.univ-amu.fr>
2018-12-05 11:29:45 +0100

    Ajout de la classe Rectangle

---
+++ src/main/java/image/Rectangle.java
@@ -0,0 +1,30 @@
+package image;
+
+import image.Shape;
+import javafx.scene.paint.Color;
+
+public class Rectangle implements Shape {
+
+    int x, y , width, height;
+    Color color;
+
+    public Rectangle(int x, int y, int width, int height, Color color){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+
+
+    @Override
+    public boolean contains(Point point) {
+        return (point.x < x + width && point.x > x
+            && point.y > y - height && point.y > y);
+    }
+
+    @Override
+    public Color getColor() {
+        return color;
+    }
+}
